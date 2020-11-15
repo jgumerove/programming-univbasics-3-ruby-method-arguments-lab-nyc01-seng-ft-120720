43d6ab1b11db748c071fccd52f65411a81b5d227
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-15 06:26:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,6 @@ def introduction_with_language(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Josh, language)
 
 
 
